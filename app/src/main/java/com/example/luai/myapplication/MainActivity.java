0925c661888048b56f0b48928d0321d87d221e56
Luaigehad <luaigehad@gmail.com>
2021-03-25 16:12:37 +0200

    Hello2

--- app/src/main/java/com/example/luai/myapplication/MainActivity.java
+++ app/src/main/java/com/example/luai/myapplication/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
     EditText input;
     ArrayAdapter adapter;
+    EditText erase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +32,42 @@ public class MainActivity extends AppCompatActivity {
         listwiew=(ListView)findViewById(R.id.listview);
         list= new ArrayList<String>();
         adapter =new ArrayAdapter<String> (getApplicationContext(), android.R.layout.simple_list_item_1,list);
-
-
         input=(EditText)findViewById(R.id.input);
+       erase=(EditText)findViewById(R.id.input);
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String names= input.getText().toString();
                 list.add(names);
                 listwiew.setAdapter(adapter);
-                adapter.notifyDataSetChanged();
+
+                listwiew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Toast.makeText(MainActivity.this, "name  "+list.get(position) , Toast.LENGTH_SHORT).show();
+
+
+                        adapter.notifyDataSetChanged();
+
+
+                    }
+                });
+
+                listwiew.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        Toast.makeText(MainActivity.this, list.remove(position), Toast.LENGTH_LONG).show();
+                        // list.remove(position);
+                        adapter.notifyDataSetChanged();
+                        return false;
+
+                    }
+                });
+
+                erase.setText("");
 
             }
         });
